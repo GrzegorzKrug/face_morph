55efa6607f7bfdfa45b8a4a83e310471d83e9d14
Greg <grzesiekk321@gmail.com>
2020-11-03 17:34:40 +0100

    Corner detection + directory for src

--- modules/open_cv/corners_detections.py
+++ modules/open_cv/corners_detections.py
@@ -1,16 +1,16 @@
 import numpy as np
 import cv2
 
-image = cv2.imread("shapes.png")
+image = cv2.imread("src_images/kitchen-chef.jpg")
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image_gray = np.float32(image_gray)
 
-corners = cv2.goodFeaturesToTrack(image_gray, 1000, 0.01, 10)
+corners = cv2.goodFeaturesToTrack(image_gray, 10000, 0.01, 10)
 corners = np.int0(corners)
 
 for corner in corners:
     x, y = corner.ravel()
-    cv2.circle(image, (x, y), 3, (0, 255, 0), 2)
+    cv2.circle(image, (x, y), 3, (0, 255, 0), 1)
 
 cv2.imshow("Image", image)
 cv2.waitKey()
